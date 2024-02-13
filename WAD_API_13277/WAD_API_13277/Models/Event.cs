0af0a486d90f59d1bb0428d989823bbbba4e082f
Fazliddin00013277 <wiut13277@gmail.com>
2024-02-13 18:05:13 +0500

    Define models

---
+++ WAD_API_13277/WAD_API_13277/Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace WAD_API_13277.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Type Type { get; set; }
+    }
+}
---
+++ WAD_API_13277/WAD_API_13277/Models/Type.cs
@@ -0,0 +1,9 @@
+﻿namespace WAD_API_13277.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
