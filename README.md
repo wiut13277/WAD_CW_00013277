8ccee8c1bffdc73706ca489b82df0cdc81497c05
Fazliddin00013277 <wiut13277@gmail.com>
2024-02-13 18:07:50 +0500

    Fix calculation and models

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  
 ## This application was developed for Web
 ## Application module, as coursework portfolio project @ WIUT by student ID: 00013277
-### Calculations - 13277 / 20 = 663.85. Last digit of remainder is 5
+### Calculations - 13277 / 20 = 663.85. Remainder would be 17
--- WAD_API_13277/WAD_API_13277/Models/Feedback.cs
+++ WAD_API_13277/WAD_API_13277/Models/Feedback.cs
@@ -1,9 +1,9 @@
 ﻿namespace WAD_API_13277.Models
 {
-    public class Event
+    public class Feedback
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public Type Type { get; set; }
     }
