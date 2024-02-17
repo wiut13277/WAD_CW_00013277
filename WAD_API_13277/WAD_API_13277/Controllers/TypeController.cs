c52d1efb6855106dd18a9ec7d345ef920daa5d9a
wiut132777 <113832224+wiut13277@users.noreply.github.com>
2024-02-18 00:02:58 +0500

    Create controller for feedbacktype

--- WAD_API_13277/WAD_API_13277/Controllers/TypeController.cs
+++ WAD_API_13277/WAD_API_13277/Controllers/TypeController.cs
@@ -1,12 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using WAD_DAL_13277.Models;
+using WAD_DAL_13277.Repositories;
 
 namespace WAD_API_13277.Controllers
 {
-    internal class TypeController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeController : ControllerBase
     {
+        private readonly ICRUDRepository<FeedbackType> _repository;
+
+        public TypeController(ICRUDRepository<FeedbackType> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_repository.GetAll());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_repository.GetById(id));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(FeedbackType type, int id)
+        {
+            try
+            {
+                _repository.Update(type, id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(FeedbackType type)
+        {
+            try
+            {
+                _repository.Create(type);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _repository.Delete(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
     }
 }
